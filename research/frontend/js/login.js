baa1eb41fe40bc784270d387f722d78f79120292
swzoey <tmddnnim2da@gmail.com>
2018-01-09 02:41:01 -0800

    Make login.html, login.js, login.css for test with back-end
    Signed-off-by: swzoey <tmddnnim2da@gmail.com>

---
+++ research/frontend/js/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("#submit").click(function () {
+        //collect userName and password entered by users
+        var userName = $("#username").val();
+        var password = $("#password").val();
+
+        auth(userName, password);
+    });
+});
+
+function auth(userName, password) {
+    $.ajax
+    ({
+        type: "POST",
+        //SEND TO MY SERVER URL
+        url: "eomserver",
+        dataType: 'json',
+        async: false,
+        data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
+        success: function (response) {
+          alert(JSON.stringify(response));
+        }
+    })
+}
