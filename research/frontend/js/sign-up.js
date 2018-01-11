9e9f73ea4e9c582012a0d9ff98d46d7d62110239
swzoey <tmddnnim2da@gmail.com>
2018-01-10 17:47:34 -0800

    Create login page files

--- research/frontend/js/sign-up.js
+++ research/frontend/js/sign-up.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
-    $("#signin").click(function () {
+    $("#submit").click(function () {
         //collect userName and password entered by users
+        var firstname = $("#firstname").val();
+        var lastname = $("#lastname").val();
         var username = $("#username").val();
         var password = $("#password").val();
-
-        auth(userName, password);
+        auth(firstname, lastname, userName, password);
     });
 });
 
-function auth(userName, password) {
+function auth(firstname, lastname, userName, password) {
     $.ajax({
         type: "POST",
         //SEND TO MY SERVER URL
         url: "eomserver",
         dataType: 'json',
         async: false,
-        data: '{"username": "' + username + '", "password" : "' + password + '"}',
+        data: '{"firstname": "' + firstname + '", "lastname" : "' + lastname + '", "username" : "' + username + '", "password" : "' + password + '"}',
         success: function (response) {
             alert(JSON.stringify(response));
         }
     })
 }
+
