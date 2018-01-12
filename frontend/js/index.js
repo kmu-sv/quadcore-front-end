41696cb2b43a6196abcd6441c6ecff8b17e589aa
swzoey <tmddnnim2da@gmail.com>
2018-01-12 15:03:33 -0800

    Update files
    Signed-off-by: swzoey <tmddnnim2da@gmail.com>

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+  //Save and modify user's interest
   $(".modal").modal();
 
   $("#edit-interests").click(function (e) {
@@ -24,7 +26,7 @@ $(document).ready(function () {
   });
   $('.chips-autocomplete').material_chip({
     autocompleteOptions: {
-      data: {
+      data: { 
         'Apple': null,
         'Microsoft': null,
         'Google': null,
@@ -37,20 +39,41 @@ $(document).ready(function () {
     }
   });
 
+  // Real time feed
   var options = [{
-    selector: '#staggered-test',
-    offset: 300,
-    callback: function (el) {
-      Materialize.showStaggeredList($(el));
-    }
-  }, {
-    selector: '#image-test',
-    offset: 300,
-    callback: function (el) {
-      Materialize.fadeInImage($(el));
-    }
-  }];
+      selector: '#staggered-test',
+      offset: 500,
+      callback: function (el) {
+        Materialize.showStaggeredList($(el));
+      }
+    }, {
+      selector: '#image-test',
+      offset: 500,
+      callback: function (el) {
+        Materialize.fadeInImage($(el));
+      }
+    }];
 
   Materialize.scrollFire(options);
-});
 
+  // Detect sroll end
+  var _throttleTimer = null;
+  var _throttleDelay = 100;
+  var $window = $(window);
+  var $document = $(document);
+
+  $window
+          .off('scroll', ScrollHandler)
+          .on('scroll', ScrollHandler);
+
+  function ScrollHandler(e) {
+      clearTimeout(_throttleTimer);
+      _throttleTimer = setTimeout(function () {
+          console.log('scroll');
+          if ($window.scrollTop() + $window.height() > $document.height() - 100) {
+              alert("getting feed!");
+          }
+
+      }, _throttleDelay);
+    }
+  });
