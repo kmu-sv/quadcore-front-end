efde23a00045339bd80e91d67873f3ce08cef3e3
swzoey <tmddnnim2da@gmail.com>
2018-01-24 09:50:32 -0800

    Add profile modal
    Signed-off-by: swzoey <tmddnnim2da@gmail.com>

---
+++ frontend/dist/js/feed.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+    //Save and modify user's interest
+    $(".modal").modal();
+    $("#edit-profile").click(function (e) {
+      e.preventDefault();
+      $("#modal-edit-profile").modal("open");
+    });
+  
+    $('.chips').material_chip();
+    $('.chips-initial').material_chip({
+      data: [{
+        tag: 'C++',
+      }, {
+        tag: 'Python',
+      }, {
+        tag: 'Google',
+      }, {
+        tag: 'Machine Learning'
+      }],
+    });
+    $('.chips-placeholder').material_chip({
+      placeholder: 'Enter your interest',
+      secondaryPlaceholder: '+Tag',
+    });
+    $('.chips-autocomplete').material_chip({
+      autocompleteOptions: {
+        data: {
+          'Apple': null,
+          'Microsoft': null,
+          'Google': null,
+          'C++': null,
+          'Python': null,
+          'Machine Learning': null,
+        },
+        limit: Infinity,
+        minLength: 1
+      }
+    });
+    // Real time feed
+    var options = [{
+      selector: '#staggered-test',
+      offset: 500,
+      callback: function (el) {
+        Materialize.showStaggeredList($(el));
+      }
+    }, {
+      selector: '#image-test',
+      offset: 500,
+      callback: function (el) {
+        Materialize.fadeInImage($(el));
+      }
+    }];
+    Materialize.scrollFire(options);
+
+    // Detect sroll end
+    var _throttleTimer = null;
+    var _throttleDelay = 100;
+    var $window = $(window);
+    var $document = $(document);
+    $window
+      .off('scroll', ScrollHandler)
+      .on('scroll', ScrollHandler);
+  
+    function ScrollHandler(e) {
+      clearTimeout(_throttleTimer);
+      _throttleTimer = setTimeout(function () {
+        console.log('scroll');
+        if ($window.scrollTop() + $window.height() > $document.height() - 100) {
+          alert("getting feed!");
+        }
+      }, _throttleDelay);
+    }
+
+    // Add news feed
+});
+
+
+  
