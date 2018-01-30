13cc49e4405996b7d3a0a0a44fd0c799c5e640d3
harrydrippin <hj332921@gmail.com>
2018-01-30 15:44:01 -0800

    Add interest parsers for each platform
    Signed-off-by: harrydrippin <hj332921@gmail.com>

---
+++ backend/quadcore/parser/__init__.py
@@ -0,0 +1,38 @@
+import requests
+
+class Parser:
+    """
+    Superclass for interest parsers.
+    """
+    # URLs for parsing user interests.
+    urls = list()
+
+    def __init__(self, token):
+        """
+        Constructor for parsers.
+        """
+        self.token = token
+
+    def get_interests(self):
+        """
+        Function for getting interest entities.
+        Returns entity ID list.
+        """
+        sources = self.get_sources(urls)
+        interests = self.get_entities_by_sources(sources)
+        return interests
+
+    def get_sources(self, urls):
+        """
+        Fetch sources from given urls.
+        Returns given response objects.
+        """
+        sources = list()
+        for url in urls:
+            sources.append(requests.get(url))
+
+    def get_entities_by_sources(self, sources):
+        """
+        Get entities by Dandelion API.
+        """
+        return list()
