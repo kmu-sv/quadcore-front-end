69c6d0527d5604f45e6b51662b9e5302854703d1
swzoey <tmddnnim2da@gmail.com>
2018-02-01 17:12:51 -0800

    Modify layout feed.html
    Signed-off-by: swzoey <tmddnnim2da@gmail.com>

--- frontend/dist/js/feed.js
+++ frontend/dist/js/feed.js
@@ -2,13 +2,38 @@ $(document).ready(function () {
 
     var common = new Common();
     var info = {
-        "firstname" : "SeungWoo",
-        "lastname" : "Jeong",
-        "username" : "swzoey",
-        "email" : "tmddnnim2da@gmail.com",
-        "interest" : ["c++", "c", "python"]
+        "firstname": "SeungWoo",
+        "lastname": "Jeong",
+        "username": "swzoey",
+        "email": "tmddnnim2da@gmail.com",
+        "interest": ["c++", "c", "python"]
     };
-    
+    // Real time feed
+    var offset = 10;
+    var template_news = "" +
+        '<div class="article-wrapper">' +
+        '<div class="card z-depth-4 qc-article-card sticky-action">' +
+        '<div class="card-content">' +
+        '{thumbnail_wrapper}' +
+        '<span class="card-title activator grey-text text-darken-4">{title}' +
+        '</span>' +
+        '<span class="qc-article-meta">{published} / ' +
+        '<a href="{link}" target="_blank">{newspaper}</a>' +
+        '</span>' +
+        '<p class="qc-article-brief">{content}</p>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+
+    var template_thumbnail = '<div class="card-image waves-effect waves-block waves-light">' +
+        '<img class="qc-article-image activator" src="{thumbnail}">' +
+        '</div>' +
+        '<hr/>';
+
+    var $window = $(window);
+    var $document = $(document);
+    var date = null;
+
     var scrollBehaviour = function () {
         if ($window.scrollTop() + $window.height() > $document.height() - 1200) {
             feedToDom(offset);
@@ -16,7 +41,7 @@ $(document).ready(function () {
         }
     };
 
-    //  Use objects, arrays, and strings as arguments
+    // Use objects, arrays, and strings as arguments
     String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
         function () {
             "use strict";
@@ -36,33 +61,13 @@ $(document).ready(function () {
             return str;
         }
 
-    // Real time feed
-    var offset = 10;
-    var template_news = "" +
-        '<div class="article-wrapper">' +
-        '<div class="card z-depth-4 qc-article-card sticky-action">' +
-        '<div class="card-content">' +
-        '<span class="card-title activator grey-text text-darken-4">{title}' +
-        '<i class="material-icons right">more_vert</i>' +
-        '</span>' +
-        '<span class="qc-article-meta">{published} /' +
-        '<a href="{link}" target="_blank">{newspaper}</a>' +
-        '</span>' +
-        '<hr>' +
-        '{thumbnail_wrapper}' +
-        '<p class="qc-article-brief">{content}</p>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
-
-    var template_thumbnail = '<div class="card-image waves-effect waves-block waves-light">'
-    + '<img class="qc-article-image activator" src="{thumbnail}">'
-    + '</div>'
-    + '<hr>';
-
     function feedToDom(offset) {
         $window.off("scroll");
         // Loading circle show
+        var $preloader_wrapper = $(".preloader-wrapper");
+        $preloader_wrapper.css('display', 'block');
+
+        //fetch feed
         var $feed_wrapper = $(".feed-wrapper");
         var data = common.fetchFeed(offset, function (data) {
             data.articles.forEach(function (v, i, a) {
@@ -71,16 +76,16 @@ $(document).ready(function () {
                 } else {
                     v.thumbnail_wrapper = "";
                 }
+                date = new Date(v.published);
+                date = date.toDateString();
+                v.published = date.replace(/ /gi, "-");
                 $feed_wrapper.append(template_news.formatUnicorn(v));
-            }); 
+            });
             $window.on("scroll", scrollBehaviour);
             // Loading circle hide
+            $preloader_wrapper.css('display', 'none');
         });
     }
-
-    $window = $(window);
-    $document = $(document);
-
     //Detect when scrolling is near the bottom
     $window.on("scroll", scrollBehaviour);
 
